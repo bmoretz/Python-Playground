fc27e62c19f8b27c7ba2e8226fc0864c27b83783
Brandon Moretz <bmoretz@msdcapital.com>
2018-01-06 17:45:18 -0500

    Into to Python for Data Science.
    Into to Python for Data Science.

---
+++ PythonPlayground/PythonPlayground/IntoToPython/ch_01.py
@@ -0,0 +1,71 @@
+# Example, do not modify!
+print(5 / 8)
+
+# Put code below here
+5 / 8
+
+print( 7 + 10 )
+
+# Just testing division
+print(5 / 8)
+
+# Addition works too.
+print(7 + 10)
+
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years?
+print( 100 * 1.1 ** 7 )
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print( savings )
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.10
+
+# Calculate result
+result = savings * factor ** 7
+
+# Print out result
+print( result )
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of factor and savings to year1
+year1 = savings * factor
+
+# Print the type of year1
+print( type( year1 ) )
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print( doubledesc )
