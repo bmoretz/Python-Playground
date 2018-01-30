4127966136265546e0df41f01da163d0172e96cc
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-01-30 13:15:12 -0500

    Intermediate Python, Ch1.
    Intermediate Python, Ch1.

---
+++ src/DataCamp/IntermediatePython/ch_01_vis.py
@@ -0,0 +1,45 @@
+
+# Print the last item from year and pop
+print( year[ -1 ] )
+print( pop[ -1 ] )
+
+# Import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+
+plt.plot( year, pop )
+
+# Display the plot with plt.show()
+plt.show()
+
+
+# Print the last item of gdp_cap and life_exp
+print( gdp_cap[-1] )
+print( life_exp[-1] )
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot( gdp_cap, life_exp )
+
+# Display the plot
+plt.show()
+
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+# Import package
+import matplotlib.pyplot as plt
+
+# Build Scatter plot
+plt.scatter( pop, life_exp )
+
+# Show plot
+plt.show()
--- src/DataCamp/IntoToPython/ch_04.py
+++ src/DataCamp/IntoToPython/ch_04.py
@@ -197,3 +197,130 @@ print("Median height of goalkeepers: " + str(np.median(gk_heights)))
 
 # Print out the median height of other players. Replace 'None'
 print("Median height of other players: " + str(np.median(other_heights)))
+
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+
+# Build histogram with 5 bins
+plt.hist(life_exp, bins = 5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins = 20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+# Histogram of life_exp, 15 bins
+plt.hist( life_exp, bins = 15 )
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist( life_exp1950, bins = 15 )
+
+# Show and clear plot again
+plt.show()
+plt.clf()
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel( xlab )
+plt.ylabel( ylab )
+
+# Add title
+plt.title( title )
+
+# After customizing, display the plot
+plt.show()
+
+# Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000,10000,100000]
+tick_lab = ['1k','10k','100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks( tick_val, tick_lab )
+
+# After customizing, display the plot
+plt.show()
+
+# Import numpy as np
+import numpy as np
+
+# Store pop as a numpy array: np_pop
+np_pop = np.array( pop )
+
+# Double np_pop
+np_pop = np_pop * 2
+
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s = np_pop)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+
+# Display the plot
+plt.show()
+
+# Specify c and alpha inside plt.scatter()
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8 )
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
+
+# Scatter plot
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Additional customizations
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+
+# Add grid() call
+plt.grid( True )
+
+# Show the plot
+plt.show()
