0c1920d531755849b020383f84df212f075a87f3
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-05-11 08:12:23 -0400

    M6 work

--- src/Mathematics/Classes/DS400/Module 6/box_maximize.py
+++ src/Mathematics/Classes/DS400/Module 6/box_maximize.py
@@ -1,1 +1,25 @@
+# A company plans to enclose three parallel rectangular areas for sorting returned goods. 
+# The three areas are within one large rectangular area and 1400 yd of fencing is available. What is the largest total area that can be​ enclosed?
 
+from sympy import symbols, solve, diff
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = symbols( 'x y', positive = True )
+
+A = 2*x + 4*y - 1056
+
+y_eq = solve( A, y )[ 0 ]
+
+Ax = y_eq * x
+
+dA = diff( Ax, x )
+
+crit_val = solve( dA, x )[ 0 ]
+short_side = y_eq.subs( { x: crit_val } )
+total_area = Ax.subs( { x: crit_val } )
+
+if __name__ == '__main__':
+	print( 'Shorter Len: {0}'.format( short_side ) )
+	print( 'Longer side: {0}'.format( crit_val ) )
+	print( 'Total Area: {0}'.format( total_area ) )
---
+++ src/Mathematics/Classes/DS400/Module 6/salmon_population.py
@@ -0,0 +1,12 @@
+# The population of salmon next year is given by
+# f(S) = Se^r(1 - S / P ),
+# where:
+# S is this years' salmon population, 
+# P is the equilibrium population,
+# r is a constant that depends upon how fast the population grows.
+
+# Find f'(S0) and solve the equation f'( S0 ) = 1.
+# Graph f'(S0) and y = 1.
+
+# Find the population for which the maximum sustainable harvest occurs if r = 0.4 and P = 850
+
