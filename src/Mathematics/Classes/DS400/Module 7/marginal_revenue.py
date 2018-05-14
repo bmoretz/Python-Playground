5185b95efb8fd124340eb761f1b8eb181b260251
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-05-14 11:27:56 -0400

    Fixed marginal revenue.

--- src/Mathematics/Classes/DS400/Module 7/marginal_revenue.py
+++ src/Mathematics/Classes/DS400/Module 7/marginal_revenue.py
@@ -1,23 +1,22 @@
 # Find the revenue function from the marginal revenue function.
 
-from sympy import solve, symbols, lambdify, diff, integrate, exp, pprint, pretty, simplify, dsolve, Integral, Rational
+import math
+from sympy import solve, symbols, lambdify, diff, integrate, exp, pprint, pretty, simplify, Rational
 import matplotlib.pyplot as plt
 import numpy as np
 
 # The marginal revenue​ (in thousands of​ dollars) from the sale of x gadgets is given by the following function.
-x, u = symbols( 'x u' )
-mRev = 4*x*( u ) ** Rational( -3, 2)
-fU = x**2 + 28000
+x, C = symbols( 'x C' )
+mRev = 4*x*( x**2 + 26000 ) ** ( Rational( -2, 3 ) )
+
+pprint( mRev )
 
 # Find the total revenue function if the revenue from 115 gadgets is 33863
-gadgets, rev = 120, 34222 / 1000
+gadgets, rev = 115, 33863 / 1000
 
-R = Integral( mRev, x ).doit()
+R = integrate( mRev, x )
+F = solve( R - rev + C, C )
 
-dR = diff( R, x )
+fR = R + round( F[ 0 ].subs( { x: gadgets } ) ) 
 
-dR.subs( { x: gadgets, u: fU } ).evalf()
-
-fun = R - C
-
-fun.subs( { t: 9 } )
+solve( fR - 45 )[ 0 ].evalf()
