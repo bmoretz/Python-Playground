618caaab84fcbb3bdff0533e03f1e63dd3d34741
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-06-01 15:01:44 -0400

    Revert "Review work"
    This reverts commit e995e0b480b214ce92350b4dc1df9b96678e3d54.

--- src/Mathematics/Classes/DS400/Quiz/IV/p09.py
+++ src/Mathematics/Classes/DS400/Quiz/IV/p09.py
@@ -2,9 +2,7 @@ import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 from matplotlib_venn import venn3, venn3_circles
-from sympy import ( symbols, solve, FiniteSet )
 
-# Total:
 # 26 were​ tall;
 # 28 had green​ peas;
 # 37 had smooth​ peas;
@@ -16,43 +14,20 @@ C = U - N
 
 A = 7
 
-x = symbols( 'x' )
-
-total = 55
 none = 2
-
-C = total - none
 
 all = 7
 
-green_smooth = 19
-tall_green = 13
-tall_smooth = all + x
+green_smooth = 19 - all
+tall_green = 13 - all
 
-smooth = 37
-green = 28
-tall = 26
+no_peas = ( green_smooth + tall_green )
 
-U = tall + green + smooth - ( tall_green + green_smooth + tall_smooth + all )
+smooth = 37 - ( green_smooth + all )
+green = 28 - ( green_smooth + tall_green + all  )
+tall = 26 - ( tall_green + all )
 
-ts = solve( U - C, x )[ 0 ]
-
-tall_smooth = tall_smooth.subs( { x: ts } )
-
-A = all 
-
-T = tall - ( tall_smooth + tall_green - all )
-G = green - ( green_smooth + tall_green - all )
-S = smooth - ( tall_smooth + green_smooth - all )
-
-TG = tall_green
-TS = tall_smooth
-GS = green_smooth
-
-# Number of plants Tall + Smooth
-tall_smooth
-
-v = venn3( subsets=( T, TG, G, GS, S, TS, A ) )
+v = venn3( subsets=( tall, green, tall_green, smooth, green_smooth, smooth, all ) )
 
 v.get_label_by_id('A').set_text('Tall')
 v.get_label_by_id('B').set_text('Green Peas')
@@ -64,3 +39,9 @@ print( 'Plants that are not tall but have smooth AND green peas {0}'.format( SG 
 
 plt.title( "Pea Plants" )
 plt.show()
+
+tall = set( { 26 } )
+green_peas = set( { 28 } )
+smooth_peas = set( { 37 } )
+
+tall 
